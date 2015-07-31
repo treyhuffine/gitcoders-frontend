07cdb701c9d007824291ab42413f3be87c2c0aa7
Trey Huffine <treyhuffine@gmail.com>
2015-07-30 19:42:35 -0700

    Add user profile page

--- src/javascripts/Root.jsx
+++ src/javascripts/Root.jsx
@@ -6,13 +6,14 @@ import { history } from 'react-router/lib/HashHistory';
 
 import App from './App.jsx'
 import Landing from './components/Landing';
+import ProfilePage from './components/ProfilePage'
 
 export default class Root extends React.Component {
   render() {
     return (
       <Router history={history}>
         <Route name="landing" path="/" component={App}>
-
+          <Route name="profile" path="/git/:login" component={ProfilePage} />
         </Route>
       </Router>
     );
